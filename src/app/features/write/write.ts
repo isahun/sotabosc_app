a7b523b2dbbffd011591e9b59401897be5acd77e
Irene Virgínia <isahun7@gmail.com>
2026-09-21 19:13:29 +0200

    feat: load prompt in write screen

--- src/app/features/write/write.ts
+++ src/app/features/write/write.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { prompts } from '../../core/data/prompts';
+import { Prompt } from '../../core/models/prompt';
 
 @Component({
   selector: 'app-write',
@@ -6,4 +9,18 @@ import { Component } from '@angular/core';
   templateUrl: './write.html',
   styleUrl: './write.css',
 })
-export class Write {}
+export class Write {
+  private route = inject(ActivatedRoute);
+
+  prompt?: Prompt;
+
+  ngOnInit() {
+    const promptId = this.route.snapshot.queryParamMap.get('promptId');
+
+    if(promptId) {
+      this.prompt = prompts.find(
+        (prompt) => prompt.id === Number(promptId),
+      );
+    }
+  }
+}
